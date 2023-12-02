56005c84b39daed3aaa1dc0b6d9cf8b403a8a6c1
antonio <antoniolm386@gmail.com>
2023-12-02 13:52:35 -0600

    security configuration done

--- src/main/java/com/practice/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/practice/SpringSecurity/config/SecurityConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	
-
+	/* Method 1 - Básic configuration
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
@@ -20,5 +22,34 @@ public class SecurityConfig {
                 .and()
                 .formLogin(login -> login.permitAll())
                 .build();
+	} */
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		
+		return httpSecurity
+				.authorizeHttpRequests( auth -> {
+					auth.requestMatchers("/v1/index2").permitAll();
+					auth.anyRequest().authenticated();
+				})
+				.formLogin()
+					.successHandler(successHandler())	//redirect after login
+				.permitAll()
+				.and()
+				.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)		//ALWAYS, IF_REQUIRED, NEVER, STATELESS
+				.invalidSessionUrl("/login")
+				.and()
+				.build();
 	}
+
+	/*Method to manage a succesful authentication*/
+	public AuthenticationSuccessHandler successHandler() {
+		
+		return ((request, response, authentication) -> {
+			response.sendRedirect("v1/index");
+		});
+	}
+	
+	
 }
